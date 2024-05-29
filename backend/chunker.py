c2ad04505be963fab3f79ccd600b674a0ad202d3
SAISH SANTOSHKUMAR CHASKAR <102912746+saishchaskar@users.noreply.github.com>
2024-05-29 13:48:49 +0530

    updates

--- backend/chunker.py
+++ backend/chunker.py
@@ -1,6 +1,6 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def chunk_text(text, chunk_size=500, chunk_overlap=50):
+def chunk_text(text, chunk_size=600, chunk_overlap=50):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     chunks = text_splitter.split_text(text)
     return chunks
